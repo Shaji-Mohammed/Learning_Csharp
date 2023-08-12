01fd3bc1b758bedfebdeaf68c3d835ead8486261
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-08-12 20:11:16 -0300

    created a simple calculator

---
+++ Simple_Calculater/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hi, I'm a calculator.");
+        Console.Write("Enter a number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Operations available: +, -, *, /, %");
+        Console.Write("Please select one operation: ");
+        string operation = Console.ReadLine();
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (operation == "+")
+        {
+            Console.WriteLine(num1 + num2);
+        } else if (operation == "-")
+        {
+            Console.WriteLine(num1 - num2);
+        } else if (operation == "*")
+        {
+            Console.WriteLine(num1 * num2);
+        } else if (operation == "/") { 
+            Console.WriteLine(num1 / num2);
+        } else {
+            Console.WriteLine(num1 % num2);
+        }
+    }
+}
