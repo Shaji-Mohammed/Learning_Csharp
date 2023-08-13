8db57c327282d4f9bb7fc27238fa06472e16f31f
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-08-13 12:49:41 -0300

    created a sample switch statement

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number (0-6): ");
+        Console.WriteLine(getDay(Convert.ToInt32(Console.ReadLine())));
+    }
+
+    static string getDay(int dayNum)
+    {
+        string day;
+
+        switch (dayNum)
+        {
+            case 0:
+                day = "Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            default:
+                day = "Invalid day number";
+                break;
+        }
+
+        return day;
+    }
+}
