d71d15a02dd3e4fbfce1072d3b43c78b1b17a99c
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-08-13 16:34:59 -0300

    created sample Class and Objects

---
+++ ClassesAndObjects/Movies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Movies
+    {
+        private string name;
+        private string director;
+        private double IMDBrating;
+        private string ratings;
+
+        public Movies(string name, string director, double IMDBrating, string ratings)
+        {
+            this.name = name;
+            this.director = director;
+            this.IMDBrating = IMDBrating;
+            Ratings = ratings;
+        }
+
+        public string Ratings
+        {
+            get { return ratings; } 
+            set
+            {
+                if (value == "PG" || value == "PG-13" || value == "R" )
+                {
+                    ratings = value;
+                }
+                else
+                {
+                    ratings = "NR";
+                }
+            }
+        }
+
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassesAndObjects;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movies tenet = new Movies("Tenet", "C. Nolan", 9, "PG");
+        Movies bladeRunner = new Movies("Blade Runner", "R. Scott", 8.5, "PG-13");
+
+        tenet.Ratings = "R";
+        Console.WriteLine(tenet.Ratings);
+
+    }
+}
